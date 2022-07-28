cad297ce3cee1f66621034b9e190b1a7a8f0ca15
Dan <35927536+dan-mba@users.noreply.github.com>
2022-07-28 08:27:30 -0400

    cache feeds in localStorage

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>ESPN NFL News</h1>
     <DivisionSelect v-model="selected"></DivisionSelect>
     <Loading :loaded="loaded"></Loading>
+    <div v-if="updating" class="updating">Using cached RSS feed</div>
     <!-- Create an Item component for each entry in the RSS feed -->
     <Item v-for="(item, index) in items"
       :key="index"
@@ -25,23 +26,32 @@ import Loading from './components/Loading.vue'
 const selected = ref('afcsouth');
 const items = ref(null);
 const loaded = ref(false);
+const updating = ref(false);
 
-function getData() {
-  fetch('https://flannel-glade.glitch.me/?' + new URLSearchParams({
-    rss: 'http://www.espn.com/blog/feed?blog=' + selected.value
-  }).toString())
-    .then(response => response.json())
-    .then(response => {
-      items.value = response.rss.channel.item;
-      loaded.value = true;
-    })
-    .catch(error => {
-      console.log(error)
-    })
+async function getData() {
+  const rss = localStorage.getItem(`espn-nfl-rss-${selected.value}`);
+  if (rss) {
+    items.value = JSON.parse(rss);
+    loaded.value = true;
+    updating.value = true;
+  }
+
+  try {
+    const data = await fetch('https://flannel-glade.glitch.me/?' + new URLSearchParams({
+      rss: 'http://www.espn.com/blog/feed?blog=' + selected.value
+    }).toString())
+    const json = await data.json();
+    items.value = json.rss.channel.item;
+    localStorage.setItem(`espn-nfl-rss-${selected.value}`, JSON.stringify(items.value));
+    loaded.value = true;
+    updating.value = false;
+  } catch(error) {
+    console.error(error)
+  }
 }
 
-watch(selected, () => getData());
-onMounted(() => getData());
+watch(selected, async () => getData());
+onMounted(async () => getData());
 </script>
 
 <style>
@@ -58,5 +68,11 @@ onMounted(() => getData());
 
 h1 {
   font-size: 1.5em;
+}
+
+.updating {
+  font-size: 11px;
+  font-style: italic;
+  color: rgba(0,0,0,0.6);
 }
 </style>
